7cc8b5769bc8eb4746a59d67579f41c1b12fbf0d
trungle12git <trungtl124@gmail.com>
2024-11-25 15:51:44 +0700

    Tao cac Interface

---
+++ RoomRentalBank/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomRentalBank.Models;
+
+namespace RoomRentalBank.Data
+{
+    public class AppDbContext: DbContext
+    {
+
+    }
+}
---
+++ RoomRentalBank/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using RoomRentalBank.Models;
+
+namespace RoomRentalBank.Interfaces
+{
+    public interface IUserRepository
+    {
+        //Đăng nhập
+        Task<User> AuthenticateAsync(string username, string password);
+        Task<bool> IsUserExistAsync(int userId);
+
+        //Dang ky
+        Task<bool> IsUsernameExistAsync(string username);
+        Task RegisterAsync(User user);
+
+        //Quan ly tai khoan
+        Task<User> GetUserByIdAsync(int userId);
+        Task<User> GetUserByNameAsync(string username);
+        Task UpdateUserAsync(User user);
+        Task ChangePasswordAsync(int userId, string newPassword);
+        Task DeleteUserAsync(int userId);
+    }
+}

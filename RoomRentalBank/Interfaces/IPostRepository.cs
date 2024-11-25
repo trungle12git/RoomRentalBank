a4a53470a16e995edf4ffbd597ce843b25b9defe
trungle12git <trungtl124@gmail.com>
2024-11-25 16:17:04 +0700

    Hoan thanh Interfaces

---
+++ RoomRentalBank/Interfaces/IPostRepository.cs
@@ -0,0 +1,26 @@
+﻿using RoomRentalBank.Models;
+using RoomRentalBank.ViewModels;
+
+namespace RoomRentalBank.Interfaces
+{
+    public interface IPostRepository
+    {
+        // Lấy tất cả bài đăng
+        Task<IEnumerable<Post>> GetAllPostsAsync();
+
+        // Lấy bài đăng theo ID
+        Task<Post> GetPostByIdAsync(int postId);
+
+        // Lấy danh sách bài đăng theo UserId
+        Task<IEnumerable<Post>> GetPostsByUserIdAsync(int userId);
+
+        // Thêm bài đăng mới
+        Task AddPostAsync(Post post);
+
+        // Cập nhật bài đăng (sử dụng ViewModel)
+        Task<Post> UpdatePostAsync(PostUpdateViewModel post);
+
+        // Xóa bài đăng
+        Task DeletePostAsync(int postId);
+    }
+}

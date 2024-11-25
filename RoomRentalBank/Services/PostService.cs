de6977abc8fbecac1d01183d1afb574769a4abd8
trungle12git <trungtl124@gmail.com>
2024-11-25 19:41:05 +0700

    Hoan thanh PostSer

---
+++ RoomRentalBank/Services/PostService.cs
@@ -0,0 +1,55 @@
+﻿using RoomRentalBank.Models;
+using RoomRentalBank.Interfaces;
+using RoomRentalBank.ViewModels;
+
+namespace RoomRentalBank.Services
+{
+    public class PostService
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        //Lay bai dang theo Id
+        public async Task<Post?> GetPostByIdAsync(int postId)
+        {
+            return await _postRepository.GetPostByIdAsync(postId);
+        }
+
+        public async Task<IEnumerable<Post>> GetPostsByUserIdAsync(int userId)
+        {
+            return await _postRepository.GetPostsByUserIdAsync(userId);
+        }
+
+        // Thêm bài đăng mới
+        public async Task AddPostAsync(Post post)
+        {
+            await _postRepository.AddPostAsync(post);
+        }
+
+        // Cập nhật bài đăng
+        public async Task<bool> UpdatePostAsync(int postId, PostUpdateViewModel viewModel)
+        {
+            var post = await _postRepository.GetPostByIdAsync(postId);
+            if (post == null)
+                return false;
+
+            await _postRepository.UpdatePostAsync(post, viewModel);
+            return true;
+        }
+
+        // Xóa bài đăng
+        public async Task<bool> DeletePostAsync(int postId)
+        {
+            var post = await _postRepository.GetPostByIdAsync(postId);
+            if (post == null)
+                return false;
+
+            await _postRepository.DeletePostAsync(postId);
+            return true;
+        }
+    }
+}

62209fd53acec8e31f840e8253a8e80035b84af3
TruKaBa <trukaba@gmail.com>
2024-11-25 09:49:41 +0700

    Add project files.

---
+++ RoomRentalBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoomRentalBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
